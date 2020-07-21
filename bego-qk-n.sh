97ce8ab984a31a751be2cb0f40f265945c8ec72f
ZyCromerZ <neetroid97@gmail.com>
2020-07-22 04:08:11 +0700

    builder: update

--- bego-qk-n.sh
+++ bego-qk-n.sh
@@ -9,7 +9,11 @@
     Getclang
     SetClang
 
+    export PATH="$(pwd)/Getclang/bin:${PATH}"
+    export LD_LIBRARY_PATH="$(pwd)/Getclang/lib64:${LD_LIBRARY_PATH}"
+    
     GetCommit=$(git log --pretty=format:'%h' -1)
+    
     HeadCommit=$GetCommit
     MainCommit=$GetCommit
     chat_id="-1001445939232"
