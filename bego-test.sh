b0fccdefed3d26c14e2a7a0dc75fce4ba25ab076
ZyCromerZ <neetroid97@gmail.com>
2020-08-01 12:57:00 +0700

    bego-test: update

--- bego-test.sh
+++ bego-test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ################################################################################################ Memetek
-    branch="20200724/root-upstream"
+    branch="20200706/Tercydux-test"
     folder="all"
     FolderUpload="Begonia/KERNELS/EXPERIMENTod/"
     spectrumFile="bego.rc"
-    . main-d.sh "get-kernel"
+    . main-c.sh "get-kernel"
     ProjectId="zyc-files"
     Getclang
     SetClang
@@ -13,32 +13,77 @@
         HeadCommit=$GetCommit
         MainCommit=$GetCommit
         chat_id="-1001434683571"
-        TypeBuid="stock-test-upstream"
     }
     
-    git reset --hard a07f95f7dc0397ecdce36358e84af053a882f0d1 # 180
-    git cherry-pick a10ef2fbd0df2811b3bbb237c3a6e0fca08163b5 3b7025ff8f0ec816e17af928c7e22a88c046cabd && uhuy
-    build "" "tele" "-1001434683571" && makeCleanOnly && rm -rf out
+    TypeBuid="80Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
 
-    git reset --hard a2872257122a652104ca41d96c7d6d1a037acbe7 # 175
-    git cherry-pick a10ef2fbd0df2811b3bbb237c3a6e0fca08163b5 3b7025ff8f0ec816e17af928c7e22a88c046cabd && uhuy
-    build "" "tele" "-1001434683571" && makeCleanOnly && rm -rf out
+    TypeBuid="100"
+    update_file "CONFIG_HZ" "CONFIG_HZ=100" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="100Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
 
-    git reset --hard 7d1095fb74bd7386227d57e1d80fd38077bae2c2 # 170
-    git cherry-pick a10ef2fbd0df2811b3bbb237c3a6e0fca08163b5 3b7025ff8f0ec816e17af928c7e22a88c046cabd && uhuy
-    build "" "tele" "-1001434683571" && makeCleanOnly && rm -rf out
+    TypeBuid="200"
+    update_file "CONFIG_HZ" "CONFIG_HZ=200" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="200Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
 
-    git reset --hard 9eae96f9c78fbef755211890dedbd99a1ffa153c # 165
-    git cherry-pick a10ef2fbd0df2811b3bbb237c3a6e0fca08163b5 3b7025ff8f0ec816e17af928c7e22a88c046cabd && uhuy
-    build "" "tele" "-1001434683571" && makeCleanOnly && rm -rf out
+    TypeBuid="250"
+    update_file "CONFIG_HZ" "CONFIG_HZ=250" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="250Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
 
-    git reset --hard 53e45a3339ff7e9b905eb446d9286bdb10b779cb # 160
-    git cherry-pick a10ef2fbd0df2811b3bbb237c3a6e0fca08163b5 3b7025ff8f0ec816e17af928c7e22a88c046cabd && uhuy
-    build "" "tele" "-1001434683571" && makeCleanOnly && rm -rf out
+    TypeBuid="300"
+    update_file "CONFIG_HZ" "CONFIG_HZ=300" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="300Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
 
-    git reset --hard 038d5a0118dee26a7687329d5bc0b6d49db9c2d5 # 155
-    git cherry-pick a10ef2fbd0df2811b3bbb237c3a6e0fca08163b5 3b7025ff8f0ec816e17af928c7e22a88c046cabd && uhuy
-    build "" "tele" "-1001434683571" && makeCleanOnly && rm -rf out
+    TypeBuid="400"
+    update_file "CONFIG_HZ" "CONFIG_HZ=400" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="400Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
+
+    TypeBuid="500"
+    update_file "CONFIG_HZ" "CONFIG_HZ=500" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="500Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
+
+    TypeBuid="600"
+    update_file "CONFIG_HZ" "CONFIG_HZ=600" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="600Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
+
+    TypeBuid="700"
+    update_file "CONFIG_HZ" "CONFIG_HZ=700" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="700Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
+
+    TypeBuid="800"
+    update_file "CONFIG_HZ" "CONFIG_HZ=800" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="800Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
+
+    TypeBuid="900"
+    update_file "CONFIG_HZ" "CONFIG_HZ=900" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="900Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
+
+    TypeBuid="1000"
+    update_file "CONFIG_HZ" "CONFIG_HZ=1000" "./arch/arm64/configs/begonia_user_defconfig"
+    TypeBuid="1000Hz"
+    uhuy
+    build "" "tele" "-1001434683571" & wait
 
 cd ..
 rm -rf $folder
